76edbba7113c46b3085cb52b77d4f223a8262182
ausi1997.com <ausafen1018@gmail.com>
2021-08-08 11:31:02 +0530

    patient registered

--- src/components/registration/register.jsx
+++ src/components/registration/register.jsx
@@ -1,20 +1,65 @@
 import React from "react";
+import axios from "axios";
 
 import {Button , TextField } from "@material-ui/core";
 import { RadioGroup , Radio } from "@material-ui/core";
 import { FormControlLabel , FormControl , FormLabel } from "@material-ui/core";
+import { useState } from "react";
 
 
 const Register = ()=>{
+
+    const [FirstName , setFirstName] = useState('');
+    const [LastName , setLastName] = useState('');
+    const [Gender , setGender] = useState('');
+    const [Dob , setDob] = useState('');
+    const [ContactNumber , setContactNumber] = useState('');
+
+    const Submit = ()=>{
+    axios
+    .post('http://localhost:8080/fhir/Patient', {
+        resourceType : "Patient",
+        name : [
+            {
+                given:FirstName
+            },
+            {
+                family:LastName
+            }
+        ],
+      gender:Gender,
+      birthDate:Dob,
+      telecom : [
+        {
+            value:ContactNumber
+        },
+    ]
+    })
+    .then((data) => {
+      alert(
+        "Patient Registered Successfully"
+      );
+      console.log(data);
+    })
+    .catch((err) => {
+      alert(err);
+    });
+};
     return(
         <div style={{marginLeft:'20px'}}>
         <h2>Register a new Pateint</h2>
         <form className="form">
-        <TextField className="input" label="FirstName" variant="outlined" />
+        <TextField className="input" label="FirstName" value={FirstName}
+         onChange={(e) => setFirstName(e.target.value)} variant="outlined" />
+
         <br></br>
-        <TextField className="input" label="LastName" variant="outlined" />
+
+        <TextField className="input" label="LastName" value={LastName}
+        onChange={(e) => setLastName(e.target.value)}  variant="outlined" />
+
         <br></br>
-        <FormControl component="fieldset">
+
+        <FormControl component="fieldset" onChange={(e) => setGender(e.target.value)}>
         <FormLabel component="legend">Gender</FormLabel>
         <RadioGroup aria-label="gender" name="gender1">
         <div>
@@ -24,21 +69,29 @@ const Register = ()=>{
           </div>
         </RadioGroup>
       </FormControl>
+
       <br></br>
+
       <TextField
       label="Date of Birth"
       type="date"
+      value={Dob}
+      onChange={(e) => setDob(e.target.value)}
       defaultValue="2021-08-06"
       className="date"
       InputLabelProps={{
         shrink: true,
       }}
     />
+
     <br></br>
-    <TextField className="input" label="Contact Number" variant="outlined" />
+
+    <TextField className="input" label="Contact Number" value={ContactNumber}
+    onChange={(e) => setContactNumber(e.target.value)} variant="outlined" />
     <br></br>
         </form>
-        <Button variant="contained" color="primary">
+
+        <Button variant="contained" onClick={Submit} color="primary">
     Submit
   </Button>
         </div>
