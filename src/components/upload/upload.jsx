314096763b3761a1c7d409555a78b03f2a32a661
ausi1997.com <ausafen1018@gmail.com>
2021-08-14 12:37:56 +0530

    read data from file

--- src/components/upload/upload.jsx
+++ src/components/upload/upload.jsx
@@ -1,21 +1,30 @@
 import { TextField } from "@material-ui/core";
-import React, { useEffect, useState } from "react";
+import React, {useState} from "react";
 
 
 
 const Upload = ()=>{
-const [file , setFile] = useState('');
+// const [file , setFile] = useState('');
 const [jsondata , setJsonData] = useState('');
 
-useEffect(()=>{
-    const data = new FormData();
-    data.append("file" , file);
-    setJsonData(data);
-},[]);
+ const handleFileChange = (files)=>{
+    const file = files[0];
+    console.log(file);
+    const render = new FileReader();
+    render.readAsBinaryString(file);
+    render.onload = ()=>{
+        setJsonData(render.result);
+        console.log(jsondata);
+    }     
+    render.onerror = ()=>{
+        console.log('file error' , render.error);
+    }
+}
+
 console.log(jsondata);
     return(
         <div>
-        <TextField type='file' className='file' onChange={(e)=>setFile(e.target.files[0])}>
+        <TextField type='file' className='file' onChange={(e)=>handleFileChange(e.target.files)}>
         </TextField>
         </div>
     )
