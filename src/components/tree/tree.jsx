98b9e59393fff18806f59b10075943edb5694efa
ausi1997.com <ausafen1018@gmail.com>
2021-08-16 09:54:24 +0530

    readme file

--- src/components/tree/tree.jsx
+++ src/components/tree/tree.jsx
@@ -2,13 +2,12 @@ import { DomainPropTypes } from "@material-ui/pickers/constants/prop-types";
 import React from "react";
 
 
-const Tree = ({props , currentIteration = 0})=>{
+const Tree = (props , currentIteration=0)=>{
     console.log(props.data , currentIteration);
     return(
         currentIteration = 5?null:(
         <div>
         <p>{props.data.name}</p>
-        <Tree props={props} currentIteration = {++currentIteration} />
         </div>
         )
         
